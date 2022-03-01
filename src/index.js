ea23f37b017bf2897c3874c8391e163797a2d088
Robert Dawson <manatees@gmail.com>
2022-02-28 23:58:11 -0800

    Add webpack and minify JS

--- src/index.js
+++ src/index.js
@@ -1,56 +1,8 @@
-// ref: https://stackoverflow.com/a/9733420/1376091
-function luminance(r, g, b) {
-  const a = [r, g, b].map(function (v) {
-    v /= 255;
-    return v <= 0.03928
-      ? v / 12.92
-      : Math.pow((v + 0.055) / 1.055, 2.4);
-  });
-  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
-}
-
-function getContrast(rgb1, rgb2) {
-  const lum1 = luminance(rgb1[0], rgb1[1], rgb1[2]);
-  const lum2 = luminance(rgb2[0], rgb2[1], rgb2[2]);
-  const brightest = Math.max(lum1, lum2);
-  const darkest = Math.min(lum1, lum2);
-  return ((brightest + 0.05) / (darkest + 0.05)).toFixed(2);
-}
-
-// Return color contrast grade based on contrast ratio
-// Ref: https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html
-function colorContrastRating(ratio) {
-  let rating = "";
-  switch (true) {
-    case (ratio >= 7):
-      rating = "great";
-      break;
-    case (ratio >= 4.5) && (ratio < 7):
-      rating = "good";
-      break;
-    case (ratio >= 3) && (ratio < 4.5):
-      rating = "okay";
-      break;
-    case (ratio < 3):
-      rating = "poor";
-      break;
-    default:
-      break;
-  }
-  return rating;
-}
-
-// Convert HEX to RGB
-function hexToRgb(hex) {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? [
-      parseInt(result[1], 16),
-      parseInt(result[2], 16),
-      parseInt(result[3], 16),
-    ]
-    : null;
-}
+import { 
+  getContrast,
+  colorContrastRating,
+  hexToRgb
+} from './utils';
 
 // function setPageBackgroundColor() {
 //   document.body.style.backgroundColor = "yellow";
@@ -94,7 +46,7 @@ document.getElementById('eyedropper').addEventListener('click', async (event) =>
   eyeDropper.open().then(result => {
     if (chosenColors.length <= 1) {
       chosenColors.push(result.sRGBHex);
-      
+
       // Remove X background effect before adding color
       if (chosenColors.length === 1) {
         color1Element.style.background = 'transparent';
@@ -121,7 +73,7 @@ document.getElementById('eyedropper').addEventListener('click', async (event) =>
 
       const result = `<p class="result-description">${chosenColors[0]} and ${chosenColors[1]} have a contrast ratio of ${contrastRatio}, which is <strong>${rating}</strong>.</p>`;
       const resultDescription = resultElement.querySelector('.result-description');
-      
+
       if (resultDescription) {
         resultElement.removeChild(resultDescription);
       }
---
+++ src/utils.js
@@ -0,0 +1,52 @@
+function luminance(r, g, b) {
+  const a = [r, g, b].map(function (v) {
+    v /= 255;
+    return v <= 0.03928
+      ? v / 12.92
+      : Math.pow((v + 0.055) / 1.055, 2.4);
+  });
+  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+}
+
+export function getContrast(rgb1, rgb2) {
+  const lum1 = luminance(rgb1[0], rgb1[1], rgb1[2]);
+  const lum2 = luminance(rgb2[0], rgb2[1], rgb2[2]);
+  const brightest = Math.max(lum1, lum2);
+  const darkest = Math.min(lum1, lum2);
+  return ((brightest + 0.05) / (darkest + 0.05)).toFixed(2);
+}
+
+// Return color contrast grade based on contrast ratio
+// Ref: https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html
+export function colorContrastRating(ratio) {
+  let rating = "";
+  switch (true) {
+    case (ratio >= 7):
+      rating = "great";
+      break;
+    case (ratio >= 4.5) && (ratio < 7):
+      rating = "good";
+      break;
+    case (ratio >= 3) && (ratio < 4.5):
+      rating = "okay";
+      break;
+    case (ratio < 3):
+      rating = "poor";
+      break;
+    default:
+      break;
+  }
+  return rating;
+}
+
+// Convert HEX to RGB
+export function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? [
+      parseInt(result[1], 16),
+      parseInt(result[2], 16),
+      parseInt(result[3], 16),
+    ]
+    : null;
+}
